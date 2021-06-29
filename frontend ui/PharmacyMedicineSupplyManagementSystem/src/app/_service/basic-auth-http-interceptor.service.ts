b8e1e0a729be7db91b8687bc78e438b3a6ac7d7b
Mohd Kashaf Siddiqui <siddiquikashaf786@gmail.com>
2021-06-29 17:23:59 +0530

    minor changes

--- frontend ui/PharmacyMedicineSupplyManagementSystem/src/app/_service/basic-auth-http-interceptor.service.ts
+++ frontend ui/PharmacyMedicineSupplyManagementSystem/src/app/_service/basic-auth-http-interceptor.service.ts
@@ -13,7 +13,6 @@ export class BasicAuthHttpInterceptorService implements HttpInterceptor {
 
   intercept(req: HttpRequest<any>, next: HttpHandler) {
     var token=localStorage.getItem('accessToken');
-    console.log(token);
     if (localStorage.getItem('userId') && token) {
       req = req.clone({
         setHeaders: {
