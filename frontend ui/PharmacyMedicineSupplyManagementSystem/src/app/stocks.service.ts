0e32ea6d40b9539ccf93c7d94ea27bdb50367db4
Mohd Kashaf Siddiqui <siddiquikashaf786@gmail.com>
2021-06-30 12:40:40 +0530

    minor change

--- frontend ui/PharmacyMedicineSupplyManagementSystem/src/app/stocks.service.ts
+++ frontend ui/PharmacyMedicineSupplyManagementSystem/src/app/stocks.service.ts
@@ -33,6 +33,7 @@ export class StocksService {
   {
     const token=localStorage.getItem('accessToken');
     const headers = new HttpHeaders({ Authorization: `Bearer ${token}`});
+    console.log(headers.get('Authorization'));
     const url = `http://localhost:8081/api/medicine-stock/update-stock/${medicine}/${count}`;
     return this.httpClient.post(url,{headers})
   }
